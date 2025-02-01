a3a2435a00460999839b3ec67f2c5ee2f176da53
kasraalitabar <kasraalitabar@gmail.com>
2025-02-01 21:41:18 +0330

    toast

--- main.js
+++ main.js
@@ -14,3 +14,17 @@ var x = setInterval(function() {
     document.getElementById("demo").innerHTML = "FINISHED";
   }
 }, 1000);
+
+Toastify({
+  text: "شفارش شما ثبت شد",
+  duration: 3000,
+  destination: "https://github.com/apvarun/toastify-js",
+  newWindow: false,
+  close: true,
+  gravity: "top", // `top` or `bottom`
+  position: "left", // `left`, `center` or `right`
+  stopOnFocus: true, // Prevents dismissing of toast on hover
+  style: {
+    background: "linear-gradient(to right, #00b09b, #96c93d)",
+  },  onClick: function(){} // Callback after click
+}).showToast();
